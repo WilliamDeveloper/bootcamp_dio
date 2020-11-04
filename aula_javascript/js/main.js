07172ee3b17c7de5d76c05dad641fcf282efbd10
William Goulart Pacheco <gtaskwill@gmail.com>
2020-11-04 10:52:24 -0300

    # salvando pontos estudados na aula

---
+++ aula_javascript/js/main.js
@@ -0,0 +1,32 @@
+//alert('meu primeiro js');
+
+//comentario de 1 linha
+
+/*
+comentario
+de mais de uma linha
+*/
+
+
+var nome= "william pacheco";
+var idade =29;
+console.log('nome: '+nome+' idade: '+idade);
+
+var a= 10;
+var b= 11;
+console.log('a: '+a+' b: '+b+' = '+(a+b));
+
+var a= "10";
+var b= "11";
+console.log('a: '+a+' b: '+b+' = '+(a+b));
+
+var frase = "Japão é o melhor time do mundo!";
+fraseModificada = frase.replace('Japão','Brasil');
+console.log(fraseModificada);
+console.log(fraseModificada.toUpperCase());
+console.log(fraseModificada.toLowerCase());
+
+var a = 7;
+var b = 7;
+var resultado = a * b;
+console.log(resultado);
