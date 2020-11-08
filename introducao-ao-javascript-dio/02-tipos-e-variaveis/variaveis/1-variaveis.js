eb4ff6562643e8ba336a9af6e7f360782d7330b4
William Goulart Pacheco <gtaskwill@gmail.com>
2020-11-08 20:57:14 -0300

    # salvando pontos estudados na aula

--- introducao-ao-javascript-dio/02-tipos-e-variaveis/variaveis/1-variaveis.js
+++ introducao-ao-javascript-dio/02-tipos-e-variaveis/variaveis/1-variaveis.js
@@ -1,6 +1,15 @@
+
+// cria variavel no escopo global window
 var nameVar = "Guilherme";
+
+// cria variavel mas nao cria fora do escopo (nao cria no window)
 let nameLet = "Guilherme";
+
+// faz nao muda tipo primitivo (int, boolean, str)
 const nameConst = "Guilherme";
+// mas é possivel mudar conteudo de:
+// (objeto, array, funcao)
+
 
 console.log(`nameVar: ${nameVar}`);
 console.log(`nameLet: ${nameLet}`);
