6ac5c94980b22dee93d32bf37cc7d6c2cd641fe1
William Goulart Pacheco <gtaskwill@gmail.com>
2020-11-08 20:38:30 -0300

    # salvando pontos estudados na aula

---
+++ introducao-ao-javascript-dio/estrutura-repeticao/array-foreach.js
@@ -0,0 +1,17 @@
+const persons = [
+  {
+    name: "Guilherme",
+    age: 26,
+    gender: gender.MAN
+  },
+  {
+    name: "Pedro",
+    age: 43,
+    gender: gender.MAN
+  },
+  {
+    name: "Jennifer",
+    age: 18,
+    gender: gender.WOMAN
+  }
+];
