4bf015c788517a5315a8c64aef08474ed83a864d
William Goulart Pacheco <gtaskwill@gmail.com>
2020-11-08 18:15:27 -0300

    # salvando pontos estudados na aula

--- introducao-ao-javascript-dio/estrutura-repeticao/for-in-for-of.js
+++ introducao-ao-javascript-dio/estrutura-repeticao/for-in-for-of.js
@@ -1,10 +1,12 @@
 let array2 = [3,5,7];
 array2.foo = "hello";
 
+//mostra todos index ou propriedades do array
 for(let i in array2){
     console.log(i) ; // "0","1","2","foo"
 }
 
+//mostra valores de propriedades numeradas
 for(let j of array2){
     console.log(j) ; // "3","5","7",undefinied
 }
