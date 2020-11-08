b7a21b08ec81e7954415f1ee0d8187dc4a3006a1
William Goulart Pacheco <gtaskwill@gmail.com>
2020-11-08 20:53:47 -0300

    # salvando pontos estudados na aula

---
+++ introducao-ao-javascript-dio/02-tipos-e-variaveis/tipos/arrray-lista-pilha-fila.js
@@ -0,0 +1,9 @@
+const lista_usuarios = ["Guilherme", "Pedro", "Jennifer"];
+
+//adicionar elemento no fim da lista
+lista_usuarios.push("william");
+console.log('lista_usuarios ',lista_usuarios);
+
+//remover elemento do inicio da lista
+lista_usuarios.shift();
+console.log('lista_usuarios ',lista_usuarios);
