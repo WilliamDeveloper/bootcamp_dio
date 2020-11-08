7dc2b7391f09a8ca719aff8f031659965c818c1d
William Goulart Pacheco <gtaskwill@gmail.com>
2020-11-08 20:27:42 -0300

    # salvando pontos estudados na aula

---
+++ introducao-ao-javascript-dio/estrutura-repeticao/filter.js
@@ -0,0 +1,17 @@
+ const lista_estudante = [
+    {
+      name: "Grace",
+      grade: 7
+    },
+    {
+      name: "Paul",
+      grade: 4
+    },
+    {
+      name: "Jennifer",
+      grade: 10
+    }
+  ];
+
+
+const lista_estudante_filtrada = lista_estudante.filter(student => student.grade >= 7);
