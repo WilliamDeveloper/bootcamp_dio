b77cac063e28e57384a5be28d120dd83818f4f7c
William Goulart Pacheco <gtaskwill@gmail.com>
2020-11-08 17:19:42 -0300

    # salvando pontos estudados na aula

--- introducao-ao-javascript-dio/tipos-e-variaveis/variaveis/cast-negacao-boolean.js
+++ introducao-ao-javascript-dio/tipos-e-variaveis/variaveis/cast-negacao-boolean.js
@@ -1,3 +1,5 @@
+//# https://brianflove.com/2014-09-02/whats-the-double-exclamation-mark-for-in-javascript/#:~:text=If%20you%20have%20ever%20noticed,(true%20or%20false)%20value.
+
 var name2;
 resposta_bool = !!name2;
 
