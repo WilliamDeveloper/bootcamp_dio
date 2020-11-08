c7c9cf8a14e1be351990e5a928aeff168470883c
William Goulart Pacheco <gtaskwill@gmail.com>
2020-11-08 18:01:13 -0300

    # salvando pontos estudados na aula

--- introducao-ao-javascript-dio/operadores/binario.js
+++ introducao-ao-javascript-dio/operadores/binario.js
@@ -1,1 +1,9 @@
 "length" in { marca : "honda", idade : 18} // false
+"cedro" in ['cedro','laranja'] //false
+0 in ['cedro','laranja'] //true
+
+
+
+var date = new Date();
+if(date instanceof Date){} //true
+
