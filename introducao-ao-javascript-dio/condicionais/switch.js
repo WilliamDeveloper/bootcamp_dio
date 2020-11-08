766074d881f68b41b848e19d691f658ff159ff62
William Goulart Pacheco <gtaskwill@gmail.com>
2020-11-08 17:36:17 -0300

    # salvando pontos estudados na aula

---
+++ introducao-ao-javascript-dio/condicionais/switch.js
@@ -0,0 +1,13 @@
+const fruit = 'melao';
+
+switch (fruit) {
+    case 'melao':
+        console.log('melao');
+        break;
+    case 'manga':
+        console.log('melao');
+        break;
+    default:
+        console.log('opcao invalida')
+
+}
