9cc15d3530b640741486e519c9fa2d8bf74f1076
William Goulart Pacheco <gtaskwill@gmail.com>
2020-11-08 22:57:35 -0300

    # salvando pontos estudados na aula

--- introducao-ao-javascript-dio/operadores/aritmeticos.js
+++ introducao-ao-javascript-dio/operadores/aritmeticos.js
@@ -14,24 +14,30 @@
 //------------------------------------------
 
 //incremento e decremento
-++x
-x++
+// ++x
+// x++
 
---x
-x--
+// --x
+// x--
 
+//------------------------------------------
 
 //negacao e adicao
--3      //retorna 3
-+"3"   //retorna 3
-+true   //retorna 1
-+false   //retorna 0
--true //retorna -1
+// -3      //retorna 3
+// +"3"   //retorna 3
+// +true   //retorna 1
+// +false   //retorna 0
+// -true //retorna -1
 
+//------------------------------------------
 
 //operador exponenciacao
-2 **3
-10** -1
+// 2 **3
+// 10** -1
+
+//------------------------------------------
 
 //operador de agrupamento
-2 * ( 3 + 2)
+// 2 * ( 3 + 2)
+
+//------------------------------------------
