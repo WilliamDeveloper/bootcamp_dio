1acc906dcfa1f053ee28bb71901a6043845ca97d
William Goulart Pacheco <gtaskwill@gmail.com>
2020-11-08 22:56:47 -0300

    # salvando pontos estudados na aula

--- introducao-ao-javascript-dio/operadores/aritmeticos.js
+++ introducao-ao-javascript-dio/operadores/aritmeticos.js
@@ -1,5 +1,17 @@
-//modulo
-12 % 5 // retorna 2
+//modulo %
+
+// operador binario
+// operando1 operador operando2
+//1 + 2
+
+//operador unario
+// operando1 operador
+// operador operando1
+
+// operador binario . retorna o inteiro restante da divisao dos dois operandos
+//12 % 5 // retorna 2
+
+//------------------------------------------
 
 //incremento e decremento
 ++x
