2ef172bddcb99c0d0fc1e6338dad7788bb3a8bb6
William Goulart Pacheco <gtaskwill@gmail.com>
2020-11-09 14:39:58 -0300

    # salvando pontos estudados na aula

--- introducao-ao-javascript-dio/08-array-manipulacao/04-iterar-elementos.js
+++ introducao-ao-javascript-dio/08-array-manipulacao/04-iterar-elementos.js
@@ -1,5 +1,5 @@
 var lista_1 = [1,2,3,4,5,6,7,8,9];
 
-lista_1.forEach( (value,index) => {
-    console.log(`${index} : ${value}`);
-})
+lista_1.forEach( (value,index, lista_iterada) => {
+    console.log(`${index} : ${value} ${lista_iterada}`);
+});
