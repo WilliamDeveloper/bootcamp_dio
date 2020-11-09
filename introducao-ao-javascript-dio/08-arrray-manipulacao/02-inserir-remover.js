3dffe182338769d22c6acec65941f84275e15570
William Goulart Pacheco <gtaskwill@gmail.com>
2020-11-09 14:14:19 -0300

    # salvando pontos estudados na aula

---
+++ introducao-ao-javascript-dio/08-arrray-manipulacao/02-inserir-remover.js
@@ -0,0 +1,20 @@
+
+lista = ['manga','pera','pepino'];
+
+
+//[fim] adicionar no fim da lista
+lista.push('laranja');
+
+
+//[fim] remover ultimo elemento da lista
+lista.pop();
+
+
+
+lista = ['manga','pera','pepino'];
+
+// [inicio] adicionar no inicio da lista
+lista.unshift('chuchu');
+
+// [inicio] remover primeiro elemento da lista
+lista.shift();
