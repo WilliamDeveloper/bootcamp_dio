79d4db3cd3bd2529ed57cd4f4ab118a3a54734c5
William Goulart Pacheco <gtaskwill@gmail.com>
2020-11-09 14:42:22 -0300

    # salvando pontos estudados na aula

--- introducao-ao-javascript-dio/08-array-manipulacao/04-iterar-elementos.js
+++ introducao-ao-javascript-dio/08-array-manipulacao/04-iterar-elementos.js
@@ -1,8 +1,14 @@
 var lista_1 = [1,2,3,4,5,6,7,8,9];
 
+
+
+//somente itera sobre a lista
 lista_1.forEach( (value,index, lista_iterada) => {
     console.log(`${index} : ${value} ${lista_iterada}`);
 });
 
+
+
+// itera sobre os elementos da lista gerando um novo
 var lista_2 = lista_1.map( value => value *2)
 console.log(lista_2);
