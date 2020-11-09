7ab88f1e2e9862596107eb4fc3ef077ae13447a5
William Goulart Pacheco <gtaskwill@gmail.com>
2020-11-09 13:50:27 -0300

    # salvando pontos estudados na aula

--- introducao-ao-javascript-dio/07-design-patterns/mais-usados-javascript.js
+++ introducao-ao-javascript-dio/07-design-patterns/mais-usados-javascript.js
@@ -1,6 +1,9 @@
 //-----------------------------------------------------------------------------------------
 // factory
+    //  exemplo uma funcao q retorna um objeto
 // singleton
+    // exemplo jquery
 // decorator
+    //exempolo @readonly
 // observer
 // module
