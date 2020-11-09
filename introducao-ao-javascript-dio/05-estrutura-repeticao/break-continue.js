00dab2b0d1c65871db433ab7b907766ea0ec1362
William Goulart Pacheco <gtaskwill@gmail.com>
2020-11-09 08:52:16 -0300

    # salvando pontos estudados na aula

---
+++ introducao-ao-javascript-dio/05-estrutura-repeticao/break-continue.js
@@ -0,0 +1,13 @@
+// break
+for (var i =0;i < 5;i++){
+    if(true){
+        break; // vai parar o loop
+    }
+}
+
+//continue
+for (var i =0;i < 5;i++){
+    if(true){
+        break; // vai pular para o proxima iteracao do loop
+    }
+}
