8f4d97a286d657a1b90c4a3e21fa5fce0d181fa4
William Goulart Pacheco <gtaskwill@gmail.com>
2020-11-09 14:22:47 -0300

    # salvando pontos estudados na aula

--- introducao-ao-javascript-dio/08-arrray-manipulacao/03-juntar-arrays.js
+++ introducao-ao-javascript-dio/08-arrray-manipulacao/03-juntar-arrays.js
@@ -5,7 +5,7 @@ var lista_3 = lista_1.concat(lista_2);
 
 
 
-
+// fatiando array para criar um novo
 var lista_4 = [1,2,3,4,5,6,7,8,9];
 var lista_5 = lista_4.slice(0,2); // 1, 2
 var lista_5 = lista_4.slice(2,5); // 3, 4, 5
