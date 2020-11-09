404d61ee6adedda4714c33d82c4e39bac7a94002
William Goulart Pacheco <gtaskwill@gmail.com>
2020-11-09 13:27:28 -0300

    # salvando pontos estudados na aula

--- introducao-ao-javascript-dio/06-orientacao-objetos/encapsulamento.js
+++ introducao-ao-javascript-dio/06-orientacao-objetos/encapsulamento.js
@@ -18,3 +18,27 @@ p.getName();
 p.setName('william');
 
 p.name; // undefinied
+
+/////////////////////////
+'use strict';
+
+function Person(initialName){
+    var name = initialNeme;
+
+    Object.defineProperty(this,'name',{
+        get:function () {
+            return name;
+        },
+        set:function (value) {
+            name=value;
+        }
+    });
+
+}
+
+const p = new Person('Guilherme');
+
+p.getName();
+p.setName('william');
+
+p.name; // undefinied
