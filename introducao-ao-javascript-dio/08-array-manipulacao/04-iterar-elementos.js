7ef647d313dd6452b624a02a683a9dd9beeec768
William Goulart Pacheco <gtaskwill@gmail.com>
2020-11-09 15:04:26 -0300

    # salvando pontos estudados na aula

--- introducao-ao-javascript-dio/08-array-manipulacao/04-iterar-elementos.js
+++ introducao-ao-javascript-dio/08-array-manipulacao/04-iterar-elementos.js
@@ -32,6 +32,13 @@ lista_1_iterator.next()// { value : 2 , done : false }
 lista_1_iterator.next()// { value : 3 , done : true }
 
 
+// find retorna o valor do primeiro item que satisfaca a condicao
 var lista_1 = [1,2,3,4,5,6,7,8,9];
 var first_number_greater_than_5 = lista_1.find( value =>  value > 5);
 console.log(first_number_greater_than_5)
+
+
+// find retorna o valor do primeiro item que satisfaca a condicao
+var lista_1 = [1,2,3,4,5,6,7,8,9];
+var first_index_greater_than_5 = lista_1.findIndex( value =>  value > 5);
+console.log(first_index_greater_than_5)
