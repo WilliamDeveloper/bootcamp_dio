6a5582b47d35596a50472b0514c2dee2e05db23b
William Goulart Pacheco <gtaskwill@gmail.com>
2020-11-09 14:05:09 -0300

    # salvando pontos estudados na aula

--- introducao-ao-javascript-dio/08-arrray-manipulacao/01-criar-array.js
+++ introducao-ao-javascript-dio/08-arrray-manipulacao/01-criar-array.js
@@ -1,3 +1,6 @@
 // criando arrays
 const array_1 = [1,2,3,4,5];
 const array_2 = new Array(1,2,3,4,5);
+const array_2 = new Array(3);// cria um array com 3 posicoes vazio
+
+const array_3 = Array.of('a','b','c',1,2,3);
