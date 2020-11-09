63098ff3758b44ba9926496f99d0ad9d68e36ae4
William Goulart Pacheco <gtaskwill@gmail.com>
2020-11-09 13:42:58 -0300

    # salvando pontos estudados na aula

---
+++ introducao-ao-javascript-dio/07-design-patterns/tipos.js
@@ -0,0 +1,31 @@
+//-----------------------------------------------------------------------------------------
+// criacao
+    // - abstract factory
+    // - builder
+    // factory method
+    // prototype
+    // singleton
+
+//-----------------------------------------------------------------------------------------
+// estruturais
+    // adapter
+    // bridge
+    // composite
+    // decorator
+    // facade
+    // business delegate
+    // flyweight
+    // proxy
+
+//-----------------------------------------------------------------------------------------
+//comportamentais
+    // chain of responsibility
+    // command
+    // interpreter
+    // iterator
+    // mediator
+    // observer
+    // state
+    // strategy
+    // template method
+    // visitor
