482d99017d9fb82a613d4827a2c8c1e17ccbee49
William Goulart Pacheco <gtaskwill@gmail.com>
2020-11-09 13:23:47 -0300

    # salvando pontos estudados na aula

--- introducao-ao-javascript-dio/06-orientacao-objetos/modificadores-acesso.js
+++ introducao-ao-javascript-dio/06-orientacao-objetos/modificadores-acesso.js
@@ -23,3 +23,31 @@ p.name; // undefinied
 
 //----------------------------------------------
 
+// apartir da versao 12 do node
+
+
+'use strict';
+
+class Person{
+    #name = '';
+
+    constructor(initialName){
+        this.#name = initialName;
+    }
+
+    setName = function (newName) {
+        this.#name = newName;
+    }
+
+    getName = function () {
+        return this.#name;
+    }
+
+}
+
+const p = new Person('Guilherme');
+
+p.getName();
+p.setName('william');
+
+p.name; // undefinied
