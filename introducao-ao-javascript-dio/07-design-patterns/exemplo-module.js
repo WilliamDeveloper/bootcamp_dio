5b5279173e7136126dc5b824bf506acd86143f50
William Goulart Pacheco <gtaskwill@gmail.com>
2020-11-09 13:59:18 -0300

    # salvando pontos estudados na aula

--- introducao-ao-javascript-dio/07-design-patterns/exemplo-module.js
+++ introducao-ao-javascript-dio/07-design-patterns/exemplo-module.js
@@ -7,3 +7,27 @@ class Person{
 export default Person;
 
 import Person from './models/person'
+
+
+//-----------
+
+
+let name = 'default';
+
+function setName(newName) {
+    name = newName;
+}
+function getName() {
+    return name;
+}
+
+module.exports ={
+    getName,
+    setName
+};
+
+
+const { getName, setName} = require('./module-teste.js');
+console.log(getName());
+console.log(setName('william'));
+console.log(getName());
