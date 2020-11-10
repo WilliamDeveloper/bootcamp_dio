ffc8a708170a34088fbfaab9a90e122fd8f2eadf
William Goulart Pacheco <gtaskwill@gmail.com>
2020-11-09 23:33:33 -0300

    # salvando pontos estudados na aula

--- introducao-ao-javascript-dio/08-array-manipulacao/05-filter.js
+++ introducao-ao-javascript-dio/08-array-manipulacao/05-filter.js
@@ -43,3 +43,49 @@ var newPets = pets.filter( (pet) =>{
 
 console.log(newPets);
 console.log(pets);
+
+
+//----------------------
+var pets = [
+    {
+        type: 'dog',
+        name: 'bolinha',
+        age: 15,
+        weight: 30
+    },
+    {
+        type: 'cat',
+        name: 'mingal',
+        age: 6,
+        weight: 2
+    },
+    {
+        type: 'dog',
+        name: 'rex',
+        age: 4,
+        weight: 5
+    },
+    {
+        type: 'cat',
+        name: 'marrom',
+        age: 2,
+        weight: 1
+    },
+    {
+        type: 'fish',
+        name: 'gulp',
+        age: 1,
+        weight: 0.01
+    },
+    {
+        type: 'horse',
+        name: 'pe de pano',
+        age: 1,
+        weight: 0.01
+    },
+]
+
+var listaDogs = pets.filter( (pet) => {
+    return pet.type === 'dog'
+})
+console.log(listaDogs)
