d41671aa2493bc1793a48c6a0a6275dc91687ee2
William Goulart Pacheco <gtaskwill@gmail.com>
2020-11-09 22:41:10 -0300

    # salvando pontos estudados na aula

--- introducao-ao-javascript-dio/08-array-manipulacao/05-map-filter-reduce.js
+++ introducao-ao-javascript-dio/08-array-manipulacao/05-map-filter-reduce.js
@@ -6,6 +6,7 @@ for(var i =0; i< lista_1.length; i++){
         lista.push(lista_1[i]);
     }
 }
+
 // filter - devolve um array novo com todos item que passaram na funcao de teste logico
 var lista_1 = [1,2,3,4,5,6,7,8,9];
 var lista = lista_1.filter( function (elemento, index, array) {
