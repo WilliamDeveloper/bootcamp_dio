98a052d6ed289853bc0d6a371dfc53546adf3c6b
William Goulart Pacheco <gtaskwill@gmail.com>
2020-11-09 22:47:44 -0300

    # salvando pontos estudados na aula

--- introducao-ao-javascript-dio/08-array-manipulacao/05-map-filter-reduce.js
+++ introducao-ao-javascript-dio/08-array-manipulacao/05-map-filter-reduce.js
@@ -12,3 +12,20 @@ var lista_1 = [1,2,3,4,5,6,7,8,9];
 var lista = lista_1.filter( function (elemento, index, array) {
    return lista_1.indexOf(elemento) === index;
 });
+
+//------
+
+var pets=[
+    { name: 'rex', type:'dog', age:10 },
+    { name: 'miau', type:'cat', age:2 },
+    { name: 'gulp', type:'fish', age:1 }
+];
+
+console.log(pets);
+
+var newPets = pets.filter( (pet) =>{
+    return pet.age < 5;
+})
+
+console.log(newPets);
+console.log(pets);
