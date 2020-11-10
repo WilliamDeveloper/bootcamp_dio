cb6fdcfae3abd50e33729d10b8691729fb0a9b73
William Goulart Pacheco <gtaskwill@gmail.com>
2020-11-09 22:33:32 -0300

    # salvando pontos estudados na aula

--- introducao-ao-javascript-dio/08-array-manipulacao/05-map-filter-reduce.js
+++ introducao-ao-javascript-dio/08-array-manipulacao/05-map-filter-reduce.js
@@ -6,3 +6,8 @@ for(var i =0; i< lista_1.length; i++){
         lista.push(lista_1[i]);
     }
 }
+// filter
+var lista_1 = [1,2,3,4,5,6,7,8,9];
+var lista = lista_1.filter( function (elemento, index, array) {
+   return lista_1.indexOf(elemento) === index;
+});
