76d666506e3dd3285db70f083da96340c4c9aab5
William Goulart Pacheco <gtaskwill@gmail.com>
2020-11-11 16:54:04 -0300

    # salvando pontos estudados na aula

--- introducao-ao-javascript-dio/10-es7/02-EventEmitter-exclusivo-node.js
+++ introducao-ao-javascript-dio/10-es7/02-EventEmitter-exclusivo-node.js
@@ -1,3 +1,10 @@
 // EventEmitter
 
 var EventEmitter = require('events');
+var emitter = new EventEmitter();
+
+emitter.on('User logged', data =>{
+  console.log(data)
+})
+
+emitter.emit('User logged', {user:'William Pacheco'})
