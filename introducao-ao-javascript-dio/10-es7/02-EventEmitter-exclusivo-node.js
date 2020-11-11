62392cf7434660aaa3c4795b173452cca01fb5d0
William Goulart Pacheco <gtaskwill@gmail.com>
2020-11-11 17:09:06 -0300

    # salvando pontos estudados na aula

--- introducao-ao-javascript-dio/10-es7/02-EventEmitter-exclusivo-node.js
+++ introducao-ao-javascript-dio/10-es7/02-EventEmitter-exclusivo-node.js
@@ -1,4 +1,4 @@
-// EventEmitter
+// EventEmitter existe somente no node . Ele é Semelhante ao EventTarget do browser
 
 var EventEmitter = require('events');
 var emitter = new EventEmitter();
@@ -8,3 +8,22 @@ emitter.on('User logged', data =>{
 })
 
 emitter.emit('User logged', {user:'William Pacheco'})
+
+//---------------------------------------
+
+class Users extends EventEmitter{
+  userLogged(data){
+    this.emit('User logged',  data)
+  }
+}
+var user = {user:'William Pacheco'}
+var users = new Users();
+users.on('User logged', data =>{
+  console.log(data)
+})
+
+users.once('apenas_uma_Vez', data =>{
+  console.log(data)
+})
+
+users.userLogged(user)
