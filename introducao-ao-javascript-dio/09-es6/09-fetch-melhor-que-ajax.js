3ee2d3caa7d72767ad1549b9fe9adf3198f8015e
William Goulart Pacheco <gtaskwill@gmail.com>
2020-11-11 15:31:07 -0300

    # salvando pontos estudados na aula

--- introducao-ao-javascript-dio/09-es6/09-fetch-melhor-que-ajax.js
+++ introducao-ao-javascript-dio/09-es6/09-fetch-melhor-que-ajax.js
@@ -1,4 +1,19 @@
 // funciona no browser
+
+//----------------------------- 
+fetch('https://raw.githubusercontent.com/WilliamDeveloper/bootcamp_dio/main/introducao-ao-javascript-dio/09-es6/data.json')
+.then( resonseStream =>  {
+  console.log('resonseStream', resonseStream.status, resonseStream)
+  resonseStream.json()
+}
+).then(data =>{
+  console.log(data)
+})
+.catch( erro => {
+  console.log(erro)
+})
+//----------------------------- 
+
 fetch('https://viacep.com.br/ws/01001000/json/')
 .then( data => {
   console.log(data);
