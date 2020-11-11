27f676e551ed10902780fbbb8c6dfb3bf58005d9
William Goulart Pacheco <gtaskwill@gmail.com>
2020-11-11 16:12:24 -0300

    # salvando pontos estudados na aula

--- introducao-ao-javascript-dio/10-es7/01-async-await.js
+++ introducao-ao-javascript-dio/10-es7/01-async-await.js
@@ -25,12 +25,17 @@ var asyncTimer = () => new Promise( (resolve, reject) =>{
 })
 
 var simpleFunc3 = async () => {
-  var data = await asyncTimer()
-  return data;
+  var data = await asyncTimer();
+  console.log(data)
+  var dataJSON = await fetch('http://127.0.0.1:5500/09-es6/data.json')
+  .then( resStream => resStream.json())
+
+  return dataJSON;
 }
 
 
-simpleFunc3().then( data => {
+simpleFunc3()
+.then( data => {
   console.log(data)
 }).catch( error => {
   console.log(error)
