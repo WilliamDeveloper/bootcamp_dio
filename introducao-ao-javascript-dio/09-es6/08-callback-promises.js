16ff7e84ef926d8e649aea85d6e3a9def2f195da
William Goulart Pacheco <gtaskwill@gmail.com>
2020-11-11 13:07:24 -0300

    # salvando pontos estudados na aula

--- introducao-ao-javascript-dio/09-es6/08-callback-promises.js
+++ introducao-ao-javascript-dio/09-es6/08-callback-promises.js
@@ -1,4 +1,68 @@
 //promises
+var myPromise3 = () => new Promise( (resolve, reject) =>{
+  // throw new Error('ocorreu um erro');
+  setTimeout(function() {
+    //faz alguma coisa
+    resolve('first data');
+  }, 1000);
+})
+
+
+var myPromise4 = () => new Promise( (resolve, reject) =>{
+  setTimeout(function() {
+    //faz alguma coisa
+    resolve('second data');
+  }, 1000);
+})
+
+console.log('teste')
+myPromise3()
+.then( data => { console.log(data); return myPromise4(); } )
+.then( data2 => console.log('data2: ',data2) )
+.catch(function(error) {
+  console.log('error2: ',error)
+})
+
+
+//---------------------------------------------------
+var myPromise1 = new Promise( (resolve, reject) =>{
+  // throw new Error('ocorreu um erro');
+  setTimeout(function() {
+    //faz alguma coisa
+    resolve('first data');
+  }, 1000);
+})
+
+
+var myPromise2 = new Promise( (resolve, reject) =>{
+  setTimeout(function() {
+    //faz alguma coisa
+    resolve('second data');
+  }, 1000);
+})
+
+// myPromise1
+//   .then(function(response){
+//     console.log('response: ',response)
+//   }).catch(function(error) {
+//     console.log('error: ',error)
+//   })
+
+  console.log('teste')
+  myPromise1
+  .then( data => { console.log(data); return myPromise2; } )
+  .then( data2 => console.log('data2: ',data2) )
+  .catch(function(error) {
+    console.log('error2: ',error)
+  })
+
+// promise - status :
+// pending - pendente excecutando
+// fullfilled
+// rejected
+
+
+//---------------------------------------------------
 //callbacks
 function doSomeThing(callback){
   setTimeout(function() {
