bf6c692793ee8a23377645b8706868f2cfd43f3f
William Goulart Pacheco <gtaskwill@gmail.com>
2020-11-11 16:03:43 -0300

    # salvando pontos estudados na aula

---
+++ introducao-ao-javascript-dio/10-es7/01-async-await.js
@@ -0,0 +1,37 @@
+//es7 - async / await
+var simpleFunc1 = async () => {
+  return 123;
+}
+
+var simpleFunc2 = async function() {
+  throw new Error('oh no')
+  return 456;
+}
+
+simpleFunc1().then( data => {
+  console.log(data)
+})
+simpleFunc2().then( data => {
+  console.log(data)
+}).catch( error => {
+  console.log(error)
+})
+
+//--------------------------------------
+var asyncTimer = () => new Promise( (resolve, reject) =>{
+  setTimeout( () => { 
+    resolve('4567897897')
+  } , 1000)
+})
+
+var simpleFunc3 = async () => {
+  var data = await asyncTimer()
+  return data;
+}
+
+
+simpleFunc3().then( data => {
+  console.log(data)
+}).catch( error => {
+  console.log(error)
+})
