11c7fecdc623b7bb5a74d88d0f115f0eea6759e2
William Goulart Pacheco <gtaskwill@gmail.com>
2020-11-11 22:45:25 -0300

    # salvando pontos estudados na aula

--- testes_node/01-testando-mocha/test/math.spec.js
+++ testes_node/01-testando-mocha/test/math.spec.js
@@ -69,14 +69,23 @@ describe('Math class', function () {
 
    it.only('teste com mock ',function(){
       const req = {};
+      // const res = {
+      //    load: sinon.spy()
+      // };
       const res = {
-         load: sinon.spy()
+         load: function(){
+            console.log('Called');
+         }
       };
+
+      sinon.stub(res, 'load').returns('xpto');
 
       const math = new Math();
       math.printSum(req,res,5,5)
 
       expect(res.load.calledOnce).to.be.true;
+      expect(res.load.args[0][0]).to.equal('index');
+      expect(res.load.args[0][1]).to.equal(10);
 
    })
 
