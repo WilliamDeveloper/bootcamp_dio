b4c13952dfd95c8379565adf7064fa9ecb523c90
William Goulart Pacheco <gtaskwill@gmail.com>
2020-11-13 23:49:35 -0300

    # salvando pontos estudados na aula

--- introducao_angular8/course-manager/src/app/app.module.ts
+++ introducao_angular8/course-manager/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
@@ -19,7 +20,16 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo : 'courses',pathMatch:'full'
+      },
+      {
+        path: 'courses', component: CourseListComponent
+      }
+    ]),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
