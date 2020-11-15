4c3d7528727d0580754c11bde4579e57869e56b2
William Goulart Pacheco <gtaskwill@gmail.com>
2020-11-15 14:28:15 -0300

    # salvando pontos estudados na aula

--- introducao_angular8/course-manager/src/app/app.module.ts
+++ introducao_angular8/course-manager/src/app/app.module.ts
@@ -4,14 +4,13 @@ import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { Error404Component } from './error-404/error-404.component';
+
 import { CourseModule } from './courses/course.module';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,        
-    Error404Component,    
   ],
   imports: [
     BrowserModule,    
@@ -22,9 +21,7 @@ import { CoreModule } from './core/core.module';
       {
         path: '', redirectTo : 'courses',pathMatch:'full'
       },
-      {
-        path: '**', component: Error404Component
-      },
+
     ]),
 
   ],
