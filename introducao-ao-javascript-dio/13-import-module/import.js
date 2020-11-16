ecb4f7309ad6a42e39f13e34ba0a92d6b7faf1d9
William Goulart Pacheco <gtaskwill@gmail.com>
2020-11-15 22:32:15 -0300

    # salvando pontos estudados na aula

--- introducao-ao-javascript-dio/13-import-module/import.js
+++ introducao-ao-javascript-dio/13-import-module/import.js
@@ -1,8 +1,15 @@
 import {soma} from "./module";
 import {subtracao} from "./module2";
+import * as mat from "./module3"
 
 let resultado = soma(1,2)
 console.log(resultado)
 
 let resultado = subtracao(6,2)
 console.log(resultado)
+
+console.log(mat.f1(1,2))
+console.log(mat.f2(2,2))
+console.log(mat.f3(3,2))
+console.log(mat.f4(4,2))
+console.log(mat.f1(5,2))
---
+++ introducao-ao-javascript-dio/13-import-module/module3.js
@@ -0,0 +1,15 @@
+export let f1 = function (a,b) {
+    return a+b
+}
+
+export let f2 = function (a,b) {
+    return a+b
+}
+
+export let f3 = function (a,b) {
+    return a+b
+}
+
+export let f4 = function (a,b) {
+    return a+b
+}
