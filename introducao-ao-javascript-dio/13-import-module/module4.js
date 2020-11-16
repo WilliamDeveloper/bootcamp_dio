3925d797a2ebbd554d0d8b4ae1247678857e4358
William Goulart Pacheco <gtaskwill@gmail.com>
2020-11-15 22:37:50 -0300

    # salvando pontos estudados na aula

---
+++ introducao-ao-javascript-dio/13-import-module/module4.js
@@ -0,0 +1,19 @@
+let f1 = function (a,b) {
+    return a+b
+}
+
+let f2 = function (a,b) {
+    return a+b
+}
+
+let f3 = function (a,b) {
+    return a+b
+}
+
+let f4 = function (a,b) {
+    return a+b
+}
+
+export {
+    f1,f2,f3,f4
+}
