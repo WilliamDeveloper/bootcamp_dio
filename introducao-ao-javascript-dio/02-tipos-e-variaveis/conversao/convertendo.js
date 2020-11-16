81386903cce320fb13fe5123669446b8d9316117
William Goulart Pacheco <gtaskwill@gmail.com>
2020-11-16 09:09:12 -0300

    # salvando pontos estudados na aula

--- introducao-ao-javascript-dio/02-tipos-e-variaveis/conversao/convertendo.js
+++ introducao-ao-javascript-dio/02-tipos-e-variaveis/conversao/convertendo.js
@@ -1,2 +1,8 @@
 var numero = Number.parseInt('45.5') //parseInt
 var numero2 = Number.parseFloat('45.5') // parseFloat
+
+
+var numero3 = 48.7
+
+var numeroStr1 = numero3.toString();
+var numeroStr2 = String(numero3);
